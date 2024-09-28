99061ba7d8afba301afa823f585fab71d3927d3d
shibamoto <silverbirder@gmail.com>
2024-09-28 10:23:08 +0900

    feat: next-view-transitions

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "next-view-transitions";
+
 function ArrowIcon() {
   return (
     <svg
@@ -20,7 +22,7 @@ export default function Footer() {
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
-          <a
+          <Link
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
@@ -28,10 +30,10 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
-          </a>
+          </Link>
         </li>
         <li>
-          <a
+          <Link
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
@@ -39,7 +41,7 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
-          </a>
+          </Link>
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
