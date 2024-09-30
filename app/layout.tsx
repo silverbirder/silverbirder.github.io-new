abe9bfdc1264ef339a693032f7939dddc452d3cd
shibamoto <silverbirder@gmail.com>
2024-09-30 23:29:47 +0900

    feat: update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { Zen_Kurenaido } from "next/font/google";
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -37,6 +36,12 @@ export const metadata: Metadata = {
   },
 };
 
+const noto = Zen_Kurenaido({
+  weight: ["400"],
+  style: "normal",
+  subsets: ["latin"],
+});
+
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
@@ -50,8 +55,7 @@ export default function RootLayout({
         lang="ja"
         className={cx(
           "text-black bg-white dark:text-white dark:bg-black",
-          GeistSans.variable,
-          GeistMono.variable
+          noto.className
         )}
       >
         <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
